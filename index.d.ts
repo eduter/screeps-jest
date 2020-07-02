eaf7038e91c4c35a1dc6b37d85a5c365e4f8d232
brisberg <brandon.risberg@gmail.com>
2020-07-02 08:47:12 -0700

    Changed library export of TestEnvironment from '* export' to 'default import'

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,3 @@
-export { mockGlobal, mockInstanceOf, mockStructure } from "./src/mocking";
-export * from "./src/TestEnvironment";
+import TestEnvironment from './src/TestEnvironment';
+export { mockGlobal, mockInstanceOf, mockStructure } from './src/mocking';
+export default TestEnvironment;
--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const TestEnvironment_1 = __importDefault(require("./src/TestEnvironment"));
 var mocking_1 = require("./src/mocking");
 Object.defineProperty(exports, "mockGlobal", { enumerable: true, get: function () { return mocking_1.mockGlobal; } });
 Object.defineProperty(exports, "mockInstanceOf", { enumerable: true, get: function () { return mocking_1.mockInstanceOf; } });
 Object.defineProperty(exports, "mockStructure", { enumerable: true, get: function () { return mocking_1.mockStructure; } });
-__exportStar(require("./src/TestEnvironment"), exports);
+exports.default = TestEnvironment_1.default;

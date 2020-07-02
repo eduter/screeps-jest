2f30062d9d12b122c51c0739c7daae2097e4a03c
brisberg <brandon.risberg@gmail.com>
2020-07-02 08:20:16 -0700

    Removed recursive Type Definition incompatible with TypeScript 3.9.x

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 "use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var mocking_1 = require("./src/mocking");
-exports.mockGlobal = mocking_1.mockGlobal;
-exports.mockInstanceOf = mocking_1.mockInstanceOf;
-exports.mockStructure = mocking_1.mockStructure;
-__export(require("./src/TestEnvironment"));
+Object.defineProperty(exports, "mockGlobal", { enumerable: true, get: function () { return mocking_1.mockGlobal; } });
+Object.defineProperty(exports, "mockInstanceOf", { enumerable: true, get: function () { return mocking_1.mockInstanceOf; } });
+Object.defineProperty(exports, "mockStructure", { enumerable: true, get: function () { return mocking_1.mockStructure; } });
+__exportStar(require("./src/TestEnvironment"), exports);

9585c25138b27cc90d21aa4a6b47eb190562a531
brisberg <brandon.risberg@gmail.com>
2020-07-02 09:58:03 -0700

    Using importHelpers with tslib to emit smaller bundle size

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const TestEnvironment_1 = __importDefault(require("./src/TestEnvironment"));
+const tslib_1 = require("tslib");
 var mocking_1 = require("./src/mocking");
-Object.defineProperty(exports, "mockGlobal", { enumerable: true, get: function () { return mocking_1.mockGlobal; } });
-Object.defineProperty(exports, "mockInstanceOf", { enumerable: true, get: function () { return mocking_1.mockInstanceOf; } });
-Object.defineProperty(exports, "mockStructure", { enumerable: true, get: function () { return mocking_1.mockStructure; } });
-exports.default = TestEnvironment_1.default;
+exports.mockGlobal = mocking_1.mockGlobal;
+exports.mockInstanceOf = mocking_1.mockInstanceOf;
+exports.mockStructure = mocking_1.mockStructure;
+tslib_1.__exportStar(require("./src/TestEnvironment"), exports);
--- src/mocking.js
+++ src/mocking.js
@@ -1,30 +1,8 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.mockStructure = exports.mockInstanceOf = exports.mockRoomPositionConstructor = exports.mockGlobal = void 0;
-const util = __importStar(require("util"));
-const jest_mock_1 = __importDefault(require("jest-mock"));
+const tslib_1 = require("tslib");
+const util = tslib_1.__importStar(require("util"));
+const jest_mock_1 = tslib_1.__importDefault(require("jest-mock"));
 /**
  * Properties I've seen having been accessed internally by Jest's matchers and message formatters (there may be others).
  */

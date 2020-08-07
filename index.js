74b5e179eba6917412e777cbf93c6bacc0926f85
brisberg <brandon.risberg@gmail.com>
2020-08-06 23:50:35 -0700

    Rebuilt js files with

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const TestEnvironment_1 = __importDefault(require("./src/TestEnvironment"));
 var mocking_1 = require("./src/mocking");
-Object.defineProperty(exports, "mockGlobal", { enumerable: true, get: function () { return mocking_1.mockGlobal; } });
-Object.defineProperty(exports, "mockInstanceOf", { enumerable: true, get: function () { return mocking_1.mockInstanceOf; } });
-Object.defineProperty(exports, "mockStructure", { enumerable: true, get: function () { return mocking_1.mockStructure; } });
+exports.mockGlobal = mocking_1.mockGlobal;
+exports.mockInstanceOf = mocking_1.mockInstanceOf;
+exports.mockStructure = mocking_1.mockStructure;
 exports.default = TestEnvironment_1.default;

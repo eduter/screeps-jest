9ad3d013f8eb32fae5bc06354233b529f727bcc2
eduter <emterroso@gmail.com>
2021-08-30 14:45:13 +0200

    Move compiled files to /dist and exclude them from repo

--- src/TestEnvironment.d.ts
+++
@@ -1,5 +0,0 @@
-import NodeEnvironment from "jest-environment-node";
-declare class TestEnvironment extends NodeEnvironment {
-    setup(): Promise<void>;
-}
-export default TestEnvironment;
--- src/setupGlobals.d.ts
+++
@@ -1,2 +0,0 @@
-declare function setupGlobals(globalObject: any): void;
-export default setupGlobals;
